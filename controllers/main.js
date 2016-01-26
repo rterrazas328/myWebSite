372393066d10ad5f4019082f30c913be1db2b0b5
Ricardo <rt73@njit.edu>
2016-01-26 00:31:58 -0500

    Added resume and fixed some links

--- controllers/main.js
+++ controllers/main.js
@@ -9,3 +9,28 @@ exports.getHomePage = function(req, res, next){
 exports.getProjectsPage = function(req, res, next){
 	res.render('projects_list', { title: 'Ricardo\'s Projects' });
 }
+exports.getResume = function(req, res, next){
+	var filename = "Terrazas_Job_Resume.pdf";
+	var dirname = "/pdfs/";
+	file = dirname + filename;
+	var options = {
+		root: '/Users/Ricardo/Documents/Ricardo/myWebSite/public/pdfs/',
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	};
+
+	res.sendFile(filename, options, function (err) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).end();
+		}
+		else {
+			console.log('Sent:', filename);
+		}
+	});
+	//res.render('projects_list', { title: 'Ricardo\'s Projects' });
+	//res.attachment(file);
+}
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ var mainController = require('../controllers/main');
 /* GET home page. */
 router.get('/', mainController.getHomePage);
 router.get('/projects', mainController.getProjectsPage);
+router.get('/resume', mainController.getResume);
 
 module.exports = router;
